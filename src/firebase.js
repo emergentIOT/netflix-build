48c9ddea9e84c914bcb6b779e8b4f86d49c1717b
singh <aman24january@gmail.com>
2021-04-29 13:24:13 +1000

    Adding firebase credenctials

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfpVgKSdXrnIngAuUFFn59DK0ngn5TgAI",
+    authDomain: "netflix-clone-46d10.firebaseapp.com",
+    projectId: "netflix-clone-46d10",
+    storageBucket: "netflix-clone-46d10.appspot.com",
+    messagingSenderId: "712565825454",
+    appId: "1:712565825454:web:11edc118038a473e9fb23f"
+  };
